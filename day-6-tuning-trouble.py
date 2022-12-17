5192024dd249ab2c7b4af13a71975d6698a1b4d7
anastasiabizyayeva <anastasia.bizyayeva@gmail.com>
2022-12-17 14:16:51 +0000

    day 6 complete

--- day-6-tuning-trouble.py
+++ day-6-tuning-trouble.py
@@ -1,3 +1,35 @@
 
-with open("day-6-input.txt") as f:
-    instructions = f.read()
+def answer_part_one():
+    with open("day-6-input.txt") as f:
+        instructions = f.read()
+
+        letter_list = list(instructions)
+        n = len(letter_list)
+
+        for i in range(0,n):
+            window = letter_list[i:i+4]
+            if len(set(window)) == 4:
+                print(window)
+                print(i+4)
+                break
+
+        # answer is 1987
+        # time to solve was 5:00
+
+
+def answer_part_two():
+    with open("day-6-input.txt") as f:
+        instructions = f.read()
+
+        letter_list = list(instructions)
+        n = len(letter_list)
+
+        for i in range(0, n):
+            window = letter_list[i:i + 14]
+            if len(set(window)) == 14:
+                print(window)
+                print(i + 14)
+                break
+
+        # answer is 3059
+        # time to solve was 30 seconds
